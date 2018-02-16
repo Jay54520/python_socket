9b40e8dd19f9e60e5307910404280c77a69ccec8
揭思敏 <jsm0834@175game.com>
2018-02-16 21:44:55 +0800

    #14 实现非阻塞 sockets

--- server_socket.py
+++ server_socket.py
@@ -1,13 +1,19 @@
 # -*- coding: utf-8 -*-
+import select
 import socket
-import threading
 
 from my_socket import MySocket
-from settings import BIND_ADDRESS, SERVER_PORT, MAX_CONNECTIONS
+from settings import BIND_ADDRESS, SERVER_PORT, MAX_CONNECTIONS, SELECT_TIMEOUT
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setblocking(0)
 server_socket.bind((BIND_ADDRESS, SERVER_PORT))
 server_socket.listen(MAX_CONNECTIONS)
+
+potential_readers = [server_socket, ]
+potential_writers = []
+potential_errs = []
+recv_messages = {}
 
 
 def deal_client_socket(client_socket):
@@ -17,14 +23,26 @@ def deal_client_socket(client_socket):
     my_socket.socket.close()
 
 
-def client_thread(client_socket):
-    return threading.Thread(target=deal_client_socket, args=(client_socket, ))
-
-
 while True:
-    client_socket, address = server_socket.accept()
-    ct = client_thread(client_socket)
-    try:
-        ct.run()
-    except RuntimeError as e:
-        print(e)
+    ready_to_read, ready_to_write, in_error = \
+        select.select(
+            potential_readers,
+            potential_writers,
+            potential_errs,
+            SELECT_TIMEOUT)
+
+    for read_socket in ready_to_read:
+        if read_socket is server_socket:
+            client_socket, address = read_socket.accept()
+            client_socket.setblocking(0)
+            potential_readers.append(client_socket)
+            potential_writers.append(client_socket)
+        else:
+            recv_messages[read_socket] = MySocket(read_socket).my_recv()
+
+    for write_socket in ready_to_write:
+        if write_socket in recv_messages:
+            MySocket(write_socket).my_send(recv_messages[write_socket])
+            # 流程结束，从监听列表中移除
+            potential_readers.remove(write_socket)
+            potential_writers.remove(write_socket)
